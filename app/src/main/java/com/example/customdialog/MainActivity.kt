d60a74a6b8c4ef4c91c9b225ede0cca95219e545
Pearl <Pearl>
2022-10-25 04:12:59 -0700

    v1.0.13

--- app/src/main/java/com/example/customdialog/MainActivity.kt
+++ app/src/main/java/com/example/customdialog/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val CustomDialog=CustomDialog("name","desc",resources.getDrawable(R.drawable.ic_info))
+        val CustomDialog= CustomDialog("name","desc",resources.getDrawable(R.drawable.ic_info))
         CustomDialog.createDialog(this)
 
 
